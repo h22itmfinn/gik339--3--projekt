ffd6721b144f434bfb87fd1b5cd5e9135a654442
Zetoro <rasmuslaurin@hotmail.com>
2023-12-20 15:52:02 +0100

    delete alert är kvar

--- client/script.js
+++ client/script.js
@@ -5,38 +5,45 @@ const url = "http://localhost:3000/cars";
 window.addEventListener("load", fetchData);
 
 function fetchData() {
+  
   fetch(url)
     .then((result) => result.json())
     .then((cars) => {
       if (cars.length > 0) {
-        let html = `<ul class="list-group row row-cols-2">`;
+        let html = `<div class="row">`;
         cars.forEach((car) => {
           html += `
-        <li
-          class="list-group-item col-md-6 text-bg-${car.color} mb-4 rounded shadow p-3">
-          <h3>${car.id}. Regnr: ${car.regnr}. Märke: ${car.brand}. Modell: ${car.model}.</h3>
-          <p>Årsmodel: ${car.yearmodel}. Pris: ${car.price}.</p>
+        <div
+          class="col-md-3 text-bg-${car.color} mb-4 rounded shadow p-3 ms-5 mt-4">
+          <h4>${car.id}. Märke: ${car.brand}. Modell: ${car.model}.</h4>
+          <p>Årsmodel: ${car.yearmodel}. Pris: ${car.price}. Regnr: ${car.regnr}. </p>
           <div>
             <button
-              class="btn btn-secondary"
-              <button class="" onclick="setCurrentUser('${car.id}')">
+              class="btn btn-${car.color}"
+              <button onclick="setCurrentUser('${car.id}')">
               Ändra
             </button>
-            <button class="btn btn-secondary " onclick="deleteUser('${car.id}')">
+            <button id="dltBtn" class="btn btn-${car.color} " onclick="deleteUser2('${car.id}')">
               Ta bort
             </button>
           </div>
-        </li>`;
+        </div>`;
         });
-        html += `</ul>`;
-
+        html += `</div>`;
         
+
+        // document.getElementById("dltBtn").addEventListener("click", deleteUser);
+        // let dlt = document.getElementById("dltBtn");
+        // dlt.addEventListener("click", deleteUser);
 
         const listContainer = document.getElementById("listContainer");
         listContainer.innerHTML = "";
         listContainer.insertAdjacentHTML("beforeend", html);
+        // document.getElementById("dltBtn").addEventListener("click", deleteUser);
+        
       }
     });
+    
 }
 
 
@@ -57,18 +64,94 @@ function setCurrentUser(id) {
 
       localStorage.setItem("currentId", car.id);
     });
+    
 }
 
+// document.getElementById("dltBtn").addEventListener("click", deleteUser);
+
 function deleteUser(id) {
-  console.log('delete', id);
-  fetch(`${url}/${id}`, { method: 'DELETE' }).then((result) => fetchData());
+  console.log('delete',id);
+  // e.preventedDefault();
+  // console.log('delete', id);
+  fetch(`${url}/${id}`, { method: 'DELETE' })
+  .then((resonse) => console.log(response));
+  //("Bilen borttagen"))
+  // .then((result) => fetchData());
   
   
+  
+  // myFunction("Bilen borttagen.");
+}
+
+function deleteUser1(id){
+  // const fetchy = await fetch(`${url}/${id}`, { method: 'DELETE' });
+  // console.log(fetchy);
+  // myFunction(fetchy);
+
+  
+  fetch(`${url}/${id}`, { method: 'DELETE'})
+    .then(res => res.status(200)) // or res.json()
+    .then(res => console.log(res))
+    .then(()=>myFunction("Bilen borttagen"))
+    .then(()=>{
+      var millisecondsToWait = 5000;
+      setTimeout(function() {
+    // Whatever you want to do after the wait
+    }, millisecondsToWait);
+    })
+    .then((result) => fetchData());
+    
+    // return res;     
+    //myFunction("Bilen borttagen");
+  
+}
+// console.log(test)
+
+// function deleteUser(id) {
+//   // Skapa en Promise
+//   const promise = new Promise((resolve) => {
+//     // Din kod för att ta bort användaren
+//     fetch(`${url}/${id}`, { method: 'DELETE' })
+//       .then(() => resolve());
+//   });
+
+//   // Lägg till sedan din önskade åtgärd efter borttagningen
+//   promise
+//     .then(() => myFunction("Bilen borttagen"))
+//     .then(() => fetchData());
+// }
+
+
+const deleteMethod = {
+  method: 'DELETE', // Method itself
+  headers: {
+   'Content-type': 'application/json; charset=UTF-8' // Indicates the content 
+  },
+  // No need to have body, because we don't send nothing to the server.
+ }
+ // Make the HTTP Delete call using fetch api
+ function deleteUser2(id){
+ fetch(`${url}/${id}`, deleteMethod) 
+ .then((response) => console.log(response))
+ .then(data => console.log(data))
+ .then(()=>myFunction("bajs")) // Manipulate the data retrieved back, if we want to do something with it
+ .catch(err => console.log(err)); // Do something with the error
+ };
+
+ function deleteUser3(id){
+  fetch(url + "/" + id, {
+    method: 'DELETE'
+  }).then(() => {
+     console.log('removed');
+  }).catch(err => {
+    console.error(err)
+  });
 }
 
 carForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(e) {
+  
   e.preventDefault();
   const serverUserObject = {
     regnr: '', //samma namn som i databasen
@@ -84,30 +167,75 @@ function handleSubmit(e) {
   serverUserObject.price = carForm.price.value;
   serverUserObject.yearmodel = carForm.yearmodel.value;
   serverUserObject.color = carForm.color.value;
+  console.log("Testtest1")
+
   
+  // myFunction(`Bilen ${carForm.regnumber.value} har ändrats`);
 
 
   const id = localStorage.getItem("currentId");
   if (id) serverUserObject.id = id;
 
+  const method = serverUserObject.id ? "PUT" : "POST";
+
   const request = new Request(url, {
-    method: serverUserObject.id ? "PUT" : "POST",
+    // method: serverUserObject.id ? "PUT" : "POST",
+    method: method,
     headers: {
       "content-type": "application/json",
     },
     body: JSON.stringify(serverUserObject),
     
   });
+  console.log("Testtest5")
+
+  const message =
+    method === "PUT"
+      ? `Bilen ${carForm.regnumber.value} har ändrats`
+      : `Bilen ${carForm.regnumber.value} har lagts till`;
+
+  myFunction(message);
 
   fetch(request).then((response) => {
     fetchData();
     localStorage.removeItem("currentId");
     carForm.reset();
-    //document.getElementById("staticBackdrop").modal("show");  
-    //$("#staticBackdrop").modal("show");
-    //var myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
-    //myModal.show();
+
+    console.log("Testtest2")
+    // showAlert("Ditt meddelande har skickats!"); // Visa alert
+    // myFunction("Bilen uppdaterad");
+
+    // const method = serverUserObject.id ? "PUT" : "POST";
+    // myFunction(`Bilen ${carForm.regnumber.value} har ändrats`, method);
+
+    // myFunction(`Bilen ${carForm.regnumber.value} har ändrats`);
+    
+
   });
+
+  console.log("Testtest3")
   
 }
 
+
+
+
+
+
+function myFunction(message) {
+    // event.defaultPrevented();
+    alert(message)
+    // alert(`${message} (${method})`);
+    // console.log(response)
+   
+}
+
+
+// function myFunction(message) {
+//   Swal.fire({
+//       icon: 'success',
+//       title: message,
+//       showConfirmButton: false,
+//       timer: 5000 // Tid i millisekunder (justera efter behov)
+//   });
+// }
--- server/cars.sql
+++ server/cars.sql
@@ -8,13 +8,13 @@ CREATE TABLE IF NOT EXISTS cars(
   ,yearmodel INTEGER NOT NULL
   ,color     VARCHAR(6) NOT NULL
 );
-INSERT INTO cars(id,regnr,brand,model,price,yearmodel,color) VALUES (1,'MZU512','Volvo','XC70',270000,2016,'black');
-INSERT INTO cars(id,regnr,brand,model,price,yearmodel,color) VALUES (2,'ERF123','Kia','Ceed',27000,2010,'yellow');
-INSERT INTO cars(id,regnr,brand,model,price,yearmodel,color) VALUES (3,'OBJ896','Volksvagen','Passat',296000,2019,'blue');
-INSERT INTO cars(id,regnr,brand,model,price,yearmodel,color) VALUES (4,'GRE324','Volvo','740',120000,1999,'blue');
-INSERT INTO cars(id,regnr,brand,model,price,yearmodel,color) VALUES (5,'OFC407','Kia','Ceed',120000,2016,'black');
-INSERT INTO cars(id,regnr,brand,model,price,yearmodel,color) VALUES (6,'WYL325','Kia','Rio',89000,2004,'black');
-INSERT INTO cars(id,regnr,brand,model,price,yearmodel,color) VALUES (7,'PRO309','Audi','Q7',95000,2009,'black');
+INSERT INTO cars(id,regnr,brand,model,price,yearmodel,color) VALUES (1,'MZU512','Volvo','XC70',270000,2016,'dark');
+INSERT INTO cars(id,regnr,brand,model,price,yearmodel,color) VALUES (2,'ERF123','Kia','Ceed',27000,2010,'warning');
+INSERT INTO cars(id,regnr,brand,model,price,yearmodel,color) VALUES (3,'OBJ896','Volksvagen','Passat',296000,2019,'secondary');
+INSERT INTO cars(id,regnr,brand,model,price,yearmodel,color) VALUES (4,'GRE324','Volvo','740',120000,1999,'secondary');
+INSERT INTO cars(id,regnr,brand,model,price,yearmodel,color) VALUES (5,'OFC407','Kia','Ceed',120000,2016,'dark');
+INSERT INTO cars(id,regnr,brand,model,price,yearmodel,color) VALUES (6,'WYL325','Kia','Rio',89000,2004,'dark');
+INSERT INTO cars(id,regnr,brand,model,price,yearmodel,color) VALUES (7,'PRO309','Audi','Q7',95000,2009,'dark');
 
 
 select * from cars;
